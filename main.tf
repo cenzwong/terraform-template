725f29d8235700c27fc14059442040cc7fa6cb78
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-12-20 18:27:57 +0800

    update

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # subscription_id   = "<azure_subscription_id>"
+  # tenant_id         = "<azure_subscription_tenant_id>"
+  # client_id         = "<service_principal_appid>"
+  # client_secret     = "<service_principal_password>"
 }
 
 resource "azurerm_resource_group" "rg" {

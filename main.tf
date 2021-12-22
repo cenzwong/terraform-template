fd4cf28ec070099ddbb733c4aa62ecc5e1e14346
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-12-22 11:40:20 +0800

    update

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
 
-  required_version = ">= 1.1.0"
-}
 
-provider "azurerm" {
-  features {}
 
-  # subscription_id   = "<azure_subscription_id>"
-  # tenant_id         = "<azure_subscription_tenant_id>"
-  # client_id         = "<service_principal_appid>"
-  # client_secret     = "<service_principal_password>"
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
